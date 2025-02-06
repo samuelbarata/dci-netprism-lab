073ff0be79eed0e8b5d00f55243bb341147165c8
Samuel Barata <samuel@barata.pt>
2025-02-06 18:32:32 +0000

    configs

--- netprism.sh
+++ netprism.sh
@@ -1,1 +1,2 @@
 CLAB_TOPO=netprism.clab.yaml && alias netprism="docker run -t --network samuel --rm -v /etc/hosts:/etc/hosts:ro -v ${PWD}/${CLAB_TOPO}:/topo.yml samuelbarata/netprism:latest -t /topo.yml"
+CLAB_TOPO=netprism.clab.yaml && alias fcli="docker run -t --network samuel --rm -v /etc/hosts:/etc/hosts:ro -v ${PWD}/${CLAB_TOPO}:/topo.yml  ghcr.io/srl-labs/nornir-srl:latest -t /topo.yml"
