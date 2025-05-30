538201a0a529a07f04cfd5e1706a1ea54619ed69
Samuel Barata <samuel@barata.pt>
2025-05-30 17:54:23 +0100

    traffic script and iperf vrf3

--- traffic.sh
+++ traffic.sh
@@ -3,11 +3,16 @@
 set -eu
 
 #  VRF-1
-# 10.128.1.1/
+# 10.128.1.1
 # 2002::10:128:1:1
+
 # VRF-2
 # 10.128.2.2
 # 2002::10:128:2:2
+
+# VRF-3
+# 10.128.3.11
+# 2002::10:128:3:11
 
 # VRF 1
 # iperf3 -c 2002::10:128:1:1 -t 10000 -i 1 -p 5201 -B 2002::10:128:1:1 -P 32 -b 125K -M 1400 &
@@ -15,27 +20,37 @@ set -eu
 # VRF 2
 # iperf3 -c 2002::10:128:2:2 -t 10000 -i 1 -p 5201 -B 2002::10:128:2:2 -P 32 -b 125K -M 1400 &
 
+# VRF 3
+# iperf3 -c 2002::10:128:3:11 -t 10000 -i 1 -p 5201 -B 2002::10:128:3:11 -P 32 -b 125K -M 1400 &
+
 startAll() {
     echo "starting traffic on all clients"
     docker exec c3dc1 iperf3 -c 2002::10:128:1:1 -t 10000 -i 1 -p 5201 -B 2002::10:128:1:3 -P 32 -b 125K -M 1400 > /dev/null &
     docker exec c4dc1 iperf3 -c 2002::10:128:1:1 -t 10000 -i 1 -p 5202 -B 2002::10:128:1:4 -P 32 -b 125K -M 1400 > /dev/null &
-    docker exec c5dc1 iperf3 -c 2002::10:128:2:2 -t 10000 -i 1 -p 5203 -B 2002::10:128:2:5 -P 32 -b 125K -M 1400 > /dev/null &
     docker exec c6dc1 iperf3 -c 2002::10:128:1:1 -t 10000 -i 1 -p 5204 -B 2002::10:128:1:6 -P 32 -b 125K -M 1400 > /dev/null &
     docker exec c8dc2 iperf3 -c 2002::10:128:1:1 -t 10000 -i 1 -p 5205 -B 2002::10:128:1:8 -P 32 -b 125K -M 1400 > /dev/null &
+    docker exec c10dc1 iperf3 -c 2002::10:128:1:1 -t 10000 -i 1 -p 5207 -B 2002::10:128:1:10 -P 32 -b 125K -M 1400 > /dev/null &
+
+    docker exec c5dc1 iperf3 -c 2002::10:128:2:2 -t 10000 -i 1 -p 5203 -B 2002::10:128:2:5 -P 32 -b 125K -M 1400 > /dev/null &
     docker exec c9dc2 iperf3 -c 2002::10:128:2:2 -t 10000 -i 1 -p 5206 -B 2002::10:128:2:9 -P 32 -b 125K -M 1400 > /dev/null &
-    docker exec c10dc1 iperf3 -c 2002::10:128:1:1 -t 10000 -i 1 -p 5207 -B 2002::10:128:1:8 -P 32 -b 125K -M 1400 > /dev/null &
+
+    docker exec c12dc1 iperf3 -c 2002::10:128:3:11 -t 10000 -i 1 -p 5201 -B 2002::10:128:3:12 -P 32 -b 125K -M 1400 > /dev/null &
+    docker exec c13dc2 iperf3 -c 2002::10:128:3:11 -t 10000 -i 1 -p 5202 -B 2002::10:128:3:13 -P 32 -b 125K -M 1400 > /dev/null &
 }
 
-pingAll() {
-    echo "pinging all clients"
-    # docker exec c9dc2 ping --help
+pingVRF1() {
+    echo "pinging all VRF-1 clients"
     docker exec c3dc1 ping -c 10 -W 1 -6 -A 2002::10:128:1:1
     docker exec c4dc1 ping -c 10 -W 1 -6 -A 2002::10:128:1:1
-    docker exec c5dc1 ping -c 10 -W 1 -6 -A 2002::10:128:2:2
     docker exec c6dc1 ping -c 10 -W 1 -6 -A 2002::10:128:1:1
     docker exec c8dc2 ping -c 10 -W 1 -6 -A 2002::10:128:1:1
-    docker exec c9dc2 ping -c 10 -W 1 -6 -A 2002::10:128:2:2
     docker exec c10dc1 ping -c 10 -W 1 -6 -A 2002::10:128:1:1
+}
+
+pingVRF2() {
+    echo "pinging all VRF-2 clients"
+    docker exec c5dc1 ping -c 10 -W 1 -6 -A 2002::10:128:2:2
+    docker exec c9dc2 ping -c 10 -W 1 -6 -A 2002::10:128:2:2
 }
 
 pingVRF3() {
@@ -53,6 +68,8 @@ stopAll() {
     docker exec c8dc2 pkill iperf3
     docker exec c9dc2 pkill iperf3
     docker exec c10dc1 pkill iperf3
+    docker exec c12dc1 pkill iperf3
+    docker exec c13dc2 pkill iperf3
 }
 
 # Packets:
@@ -76,9 +93,17 @@ fi
 # ping all clients
 if [ $1 == "ping" ]; then
     if [ $2 == "all" ]; then
-        pingAll
+        pingVRF1
+        pingVRF2
+        pingVRF3
     fi
     if [ $2 == "vrf" ]; then
+        if [ $3 == "1" ]; then
+            pingVRF1
+        fi
+        if [ $3 == "2" ]; then
+            pingVRF2
+        fi
         if [ $3 == "3" ]; then
             pingVRF3
         fi
